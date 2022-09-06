c0f7c0c3c8cd21b9d48bee0aa4d984e234f2eeb7
fumagallilaura <la_12_msn@hotmail.com>
2022-09-06 18:07:40 -0300

    feat: add recipes, users and recipes generic routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,16 @@
 import { Router } from 'express'
 import login from './login'
 import products from './products'
-import sales from './sales'
+import recipes from './recipes'
+import users from './users'
+import lists from './lists'
 
 const router = Router()
 
 router.use('/login', login)
 router.use('/products', products)
-router.use('/sales', sales)
+router.use('/recipes', recipes)
+router.use('/users', users)
+router.use('/lists', lists)
 
 export default router

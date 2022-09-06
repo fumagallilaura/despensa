8145d6ca8618c125374766e9093533b13984c8bb
fumagallilaura <la_12_msn@hotmail.com>
2022-09-06 17:56:33 -0300

    feat: add generic products and sales

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express'
 import login from './login'
+import products from './products'
+import sales from './sales'
 
 const router = Router()
 
 router.use('/login', login)
+router.use('/products', products)
+router.use('/sales', sales)
 
 export default router
